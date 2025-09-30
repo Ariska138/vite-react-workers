1a7f25ed9d92ba288bb8b0d9d94e51b206a09a15
Ariska Hidayat <ariska138@gmail.com>
2025-09-30 11:08:17 +0700

    update server vercel

---
+++ api/index.ts
@@ -0,0 +1,4 @@
+// File: api/index.ts
+import app from '../src/api/index';
+
+export default app.fetch;
---
+++ src/api/index.ts
@@ -0,0 +1,22 @@
+import { Hono } from "hono";
+// import { streamSSE } from 'hono/streaming';
+
+const app = new Hono();
+const api = new Hono();
+
+api.get("/", (c) => c.json({ name: "Safa Framework" }));
+
+// Rute baru untuk SSE
+// api.get('/time', (c) => {
+//   return streamSSE(c, async (stream) => {
+//     while (true) {
+//       const message = `It is ${new Date().toLocaleTimeString()}`;
+//       await stream.writeSSE({ data: message });
+//       await stream.sleep(1000); // Tunggu 1 detik
+//     }
+//   });
+// });
+
+app.route("/api", api);
+
+export default app;
